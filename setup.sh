5a26e25e6a5d2bb790be5a8a16ccfa1a1c02990b
Dyuman Aditya <dyuman2001@gmail.com>
2024-08-07 17:00:28 +0200

    added dependency setup

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Update the package index
+sudo apt update
+
+# Install Gazebo packages
+sudo apt install -y ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros
+
+# Install nlohmann-json3-dev
+sudo apt install -y nlohmann-json3-dev
+
+# Install TurtleBot3 packages
+sudo apt install -y ros-humble-turtlebot3*
+
+# Install Python3 and pip
+sudo apt install -y python3 python3-pip
+
+# Install Python packages using pip
+pip3 install -U jax
+pip3 install flax
+pip3 install opencv-python
+
+# Install xterm
+sudo apt install -y xterm
+
+# Install colcon common extensions
+sudo apt install -y python3-colcon-common-extensions
+
+# Install ROS Humble image rotate package
+sudo apt install -y ros-humble-image-rotate
+
+# Install ROS Humble robot localization package
+sudo apt-get install -y ros-humble-robot-localization
+
+# Install ROS Humble RTAB-Map package
+sudo apt install -y ros-humble-rtabmap*
+
+echo "Installation of all packages for mab_ros is complete!"
